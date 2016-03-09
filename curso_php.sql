1f6bbf86547ed9cf46b3898875dcaad817f2ce11
Klebson <jasvo@hotmail.com>
2016-03-08 21:34:09 -0300

    Arquivo Musql

---
+++ curso_php.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 09-Mar-2016 às 01:24
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `curso_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `musicas`
+--
+
+CREATE TABLE IF NOT EXISTS `musicas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(40) NOT NULL,
+  `autor_id` int(45) NOT NULL,
+  `midia` int(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `autor_id` (`autor_id`,`midia`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(80) NOT NULL,
+  `senha` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
